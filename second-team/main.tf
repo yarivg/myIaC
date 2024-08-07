6f1fc7126efc2f6ba1f6cf86f803c60377173cdf
Yariv Gavriel <yarivgavriel2@gmail.com>
2024-08-07 15:41:33 +0200

    Update main.tf

--- second-team/main.tf
+++ second-team/main.tf
@@ -4,5 +4,5 @@ resource "null_resource" "some_resource" {
 resource "null_resource" "some_resource2" {
 }
 
-resource "null_resource" "some_resource2" {
+resource "null_resource" "some_resource3" {
 }
