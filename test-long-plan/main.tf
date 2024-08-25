062bc94a81a7616c29408b1bce4e19fddbe99c3c
yarivg <yarivgavriel2@gmail.com>
2024-08-25 16:35:20 +0200

    add 3000 vars to other env for performance check

--- test-long-plan/main.tf
+++ test-long-plan/main.tf
@@ -1,3 +1,11 @@
 resource "null_resource" "some_resource" {
   count = 3000
 }
+
+// create 3000 vars use the index as a suffix
+resource "env0_configuration_variable" "example" {
+  count = 3000
+  name = "example-${count.index}"
+  value = "example"
+  environment_id = '8b6ff37d-60aa-4725-a365-21d8d1784948'
+}
