349386fc3c00e58845b2e54446cdf549501384e3
yarivg <yarivgavriel2@gmail.com>
2024-08-25 16:54:39 +0200

    250 vars for real

--- test-long-plan/main.tf
+++ test-long-plan/main.tf
@@ -20,7 +20,7 @@ resource "null_resource" "some_resource" {
 
 // create 3000 vars use the index as a suffix
 resource "env0_configuration_variable" "example" {
-  count = 3000
+  count = 250
   name = "example-${count.index}"
   value = "example"
   environment_id = "8b6ff37d-60aa-4725-a365-21d8d1784948"
