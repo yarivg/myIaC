332616eee4e9581630107c3d407e0c556ecd7208
yarivg <yarivgavriel2@gmail.com>
2025-11-23 16:40:15 +0200

    add json value

--- main.tf
+++ main.tf
@@ -3,3 +3,11 @@ resource "null_resource" "some_resource" {
 
 resource "null_resource" "some_resource2" {
 }
+
+variable "json_value" {
+  type = object({
+    some_array = list(string)
+  })
+
+  default = null
+}
