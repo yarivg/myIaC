6f5e04d98b70d097ae867d2308ffbb320f886b72
yarivg <yariv.gavriel@env0.com>
2026-03-09 13:20:12 +0200

    Add null resource for ENG-1280 testing

--- main.tf
+++ main.tf
@@ -4,6 +4,9 @@ resource "null_resource" "some_resource" {
 resource "null_resource" "some_resource2" {
 }
 
+resource "null_resource" "eng_1280_test" {
+}
+
 output "a" {
   value = "a as an output"
 }
