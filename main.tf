4eb346c7a030fa50f44982367dc7b99b7aac55aa
yarivg <yariv.gavriel@env0.com>
2026-03-10 12:42:00 +0200

    Add another null resource for --all test

--- main.tf
+++ main.tf
@@ -7,6 +7,9 @@ resource "null_resource" "some_resource2" {
 resource "null_resource" "qa_pr20793" {
 }
 
+resource "null_resource" "qa_pr20793_v2" {
+}
+
 output "a" {
   value = "a as an output"
 }
